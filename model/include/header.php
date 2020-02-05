d49c98c417f76acd2dfe3e15d1c290c94ece0cdc
Stiwy Caritey <stiwyc@live.fr>
2020-02-05 11:37:44 +0100

    First midification header

--- model/include/header.php
+++ model/include/header.php
@@ -1,19 +1,18 @@
-<header class="row align-items-center"> 
-
-    <div><img id="logo_header" src="public/image/logo_gbaf.png"></div>
-
-    <?php if (isset($_SESSION['auth'])): ?>
-        <div id="header_div_auth">
-            <ul>
-                <li><img class="d-none d-sm-block" src="public/image/avatar/<?php echo $_SESSION['auth']['avatar']?>" alt="Avatar"></li>
-                <li><?=$_SESSION['auth']['firstname'] . ' ' . $_SESSION['auth']['name'] ?>
-                    <ul id="header_nav_auth">
+<header> 
+     <?php if (isset($_SESSION['auth'])): ?>
+        <nav id="menu">
+            <ul class="row align-items-center justify-content-between">
+                <li><a href="index.php"><img id="logo_header" src="public/image/logo_gbaf.png"></a></li>
+                <li>
+                    <img id="auth_avatar" src="public/image/avatar/<?php echo $_SESSION['auth']['avatar']?>" alt="Avatar"> 
+                    <?=$_SESSION['auth']['firstname'] . ' ' . $_SESSION['auth']['name'] ?>
+                    <ul id="sub_menu">
                         <li><a href="index.php?action=account">Mon profil</a></li>
                         <li><a href="index.php?action=logout">Déconnexion</a></li>
                     </ul>
                 </li>
             </ul>
-        </div>
+        </nav>
 
     <?php else: ?>
 
