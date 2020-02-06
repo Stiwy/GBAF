9f41a32731b42cf5e5cb822e9b15b9bd640d97a7
Stiwy Caritey <stiwyc@live.fr>
2020-02-06 10:07:00 +0100

    Header completed

--- model/include/header.php
+++ model/include/header.php
@@ -5,7 +5,7 @@
                 <li><a href="index.php"><img id="logo_header" src="public/image/logo_gbaf.png"></a></li>
                 <li>
                     <img id="auth_avatar" src="public/image/avatar/<?php echo $_SESSION['auth']['avatar']?>" alt="Avatar"> 
-                    <?=$_SESSION['auth']['firstname'] . ' ' . $_SESSION['auth']['name'] ?>
+                    <span class="d-none d-md-inline"><?=$_SESSION['auth']['firstname'] . ' ' . $_SESSION['auth']['name']?></span>
                     <ul id="sub_menu">
                         <li><a href="index.php?action=account">Mon profil</a></li>
                         <li><a href="index.php?action=logout">Déconnexion</a></li>
@@ -15,6 +15,15 @@
         </nav>
 
     <?php else: ?>
+        <nav id="menu">
+            <ul class="row align-items-center justify-content-between">
+                <li><a href="index.php"><img id="logo_header" src="public/image/logo_gbaf.png"></a></li>
+                <li>
+                    <a href="index.php?action=account">Mon profil</a>
+                    <a href="index.php?action=logout">Déconnexion</a>
+                </li>
+            </ul>
+        </nav>
 
     <?php endif; ?>  
 
